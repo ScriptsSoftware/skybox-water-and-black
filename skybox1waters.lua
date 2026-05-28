844b947087f11576bdc9791d8ca39f84dfad1c96
ScriptsSoftware <dereckr970@gmail.com>
2026-05-28 15:00:49 -0600

    Add skybox configuration in skybox1waters.lua

---
+++ skybox1waters.lua
@@ -0,0 +1,13 @@
+local assetId = "rbxassetid://100550877282923"
+
+sky.SkyboxBk = assetId
+sky.SkyboxDn = assetId
+sky.SkyboxFt = assetId
+sky.SkyboxLf = assetId
+sky.SkyboxRt = assetId
+sky.SkyboxUp = assetId
+
+sky.StarCount = 0
+sky.SunAngularSize = 0
+sky.MoonAngularSize = 0
+sky.CelestialBodiesShown = false
